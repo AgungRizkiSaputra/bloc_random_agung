347644f2decd7cc0a4eb2dbea457a028297ced0e
zahHhar <agungrzksptra80@gmail.com>
2025-04-25 11:08:28 +0700

    P7: Jawaban Soal 13

---
+++ lib/random_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'random_bloc.dart';
+
+class RandomScreen extends StatefulWidget {
+  const RandomScreen({super.key});
+
+  @override
+  State<RandomScreen> createState() => _RandomScreenState();
+}
+
+class _RandomScreenState extends State<RandomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamHomepage();
+  }
+}
+
+class StreamHomepage extends StatefulWidget {
+  const StreamHomepage({super.key});
+
+  @override
+  State<StreamHomepage> createState() => _StreamHomePage();
+}
+
+class _StreamHomePage extends State<StreamHomepage> {
+  final _bloc = RandomNumberBloc();
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Random Number')),
+      body: Center(
+        child: StreamBuilder<int>(
+          stream: _bloc.randomNumber,
+          initialData: 0,
+          builder: (context, snapshot) {
+            return Text(
+              'Random Number: ${snapshot.data}',
+              style: const TextStyle(fontSize: 24),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _bloc.generateRandom.add(null),
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+}
